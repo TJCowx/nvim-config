4efee756a3b7578f9bafc75a612b550c8021f79c
TJCowx21 <tcowx21@gmail.com>
2024-07-03 21:50:36 -0500

    Added node_modules to telescope ignore buffer

--- init.lua
+++ init.lua
@@ -214,6 +214,9 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
+        defaults = {
+          file_ignore_patterns = { 'node_modules' }
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
