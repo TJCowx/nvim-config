d6b1c16d2f7213abfc4557eb3a1d507378867e96
TJCowx21 <tcowx21@gmail.com>
2024-07-03 21:40:21 -0500

    Updated tmux integration

--- init.lua
+++ init.lua
@@ -138,6 +138,27 @@ require('lazy').setup({
   },
 
   {
+    "aserowy/tmux.nvim",
+    config = function()
+      require("tmux").setup {
+        navigation = { -- cycles to opposite pane while navigating into the border
+          cycle_navigation = true,
+
+          -- enables default keybindings (C-hjkl) for normal mode
+          enable_default_keybindings = true,
+
+          -- prevents unzoom tmux when navigating beyond vim border
+          persist_zoom = true,
+        },
+        resize = {
+          -- enables default keybindings (A-hjkl) for normal mode
+          enable_default_keybindings = true,
+        },
+      }
+    end,  
+  },
+
+  {
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
     branch = '0.1.x',
